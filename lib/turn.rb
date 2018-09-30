a01c221a0c39befaa8d175336e6294c150a2abc1
Vladimir D <ande.vladimir@gmail.com>
2018-09-30 15:25:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,13 +31,5 @@ def position_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
+  
 end
