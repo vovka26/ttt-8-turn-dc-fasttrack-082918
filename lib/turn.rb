fad731a74c9dc41263beae3db573c1a434662c17
Vladimir D <ande.vladimir@gmail.com>
2018-09-30 14:48:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,19 @@ end
 def move(board, index, players_char = "X")
   board[index] = players_char
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+  true
+  else 
+    false
+  end
+end
+
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else 
+    true
+  end
+end
