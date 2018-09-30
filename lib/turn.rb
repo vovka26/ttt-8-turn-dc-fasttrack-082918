8a0ac63ede58387ca907018f5da7b84f201cd889
Vladimir D <ande.vladimir@gmail.com>
2018-09-30 14:44:10 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, players_char = "X")
+  board[index] = players_char
+end
